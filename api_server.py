1f770972efe10d4c48601e75a53bdb9082896e57
jody0856 <jody0856@gmail.com>
2024-10-30 10:33:29 +0700

    update api untuk training

--- api_server.py
+++ api_server.py
@@ -8,6 +8,8 @@ app = FastAPI()
 
 # Load cleaned data and models
 data_mhs = pd.read_excel('Data Mahasiswa.xlsx')
+data_krs = pd.read_excel('Data KRS Mahasiswa.xlsx')  # Memuat data KRS Mahasiswa
+data_kegiatan = pd.read_excel('Data Kegiatan Mahasiswa.xlsx')  # Memuat data kegiatan mahasiswa
 merged_data = pd.read_excel('cleaned_data.xlsx')  # Menggunakan hasil pembersihan dari cleaned_data.xlsx
 graduation_model = joblib.load('student_graduation_model.pkl')
 achievement_model = joblib.load('student_achievement_model.pkl')
@@ -31,15 +33,19 @@ def predict_student_status(data: StudentInput):
 
     # Prediksi kelulusan dengan dua fitur (Opsi 2)
     X_new_graduation = np.array([[ipk_mahasiswa, nilai_rata_rata]])
-    graduation_prob = (
-        100.0 if student_data['status_mahasiswa'].values[0].lower() == 'lulus'
-        else graduation_model.predict_proba(X_new_graduation)[0][1] * 100
-    )
+    graduation_prob = (graduation_model.predict_proba(X_new_graduation)[0][1] * 100)
 
     # Prediksi prestasi dengan tiga fitur
     X_new_achievement = np.array([[ipk_mahasiswa, nilai_rata_rata, keterlibatan_kegiatan]])
     achievement_prob = achievement_model.predict_proba(X_new_achievement)[0][1] * 100
+    
+     # Mendapatkan daftar mata kuliah yang diambil dan kode nilai
+    student_courses = data_krs[data_krs['npm_mahasiswa'] == npm][['nama_matkul','kategori_matakuliah', 'kode_nilai','total_hadir','total_terlaksana']]
+    course_list = student_courses.to_dict(orient='records')  # Mengonversi ke format yang bisa dikembalikan oleh API
 
+     # Mendapatkan daftar kegiatan yang diikuti mahasiswa
+    student_activities = data_kegiatan[data_kegiatan['npm_mahasiswa'] == npm][['nama_kegiatan', 'tingkat_kegiatan','tanggal_kegiatan']]
+    activity_list = student_activities.to_dict(orient='records')  # Mengonversi ke format yang bisa dikembalikan oleh API
     return {
         "npm_mahasiswa": npm,
         "nama_mahasiswa": student_data['nama_mahasiswa'].values[0],
@@ -48,7 +54,9 @@ def predict_student_status(data: StudentInput):
         "ipk_mahasiswa": float(ipk_mahasiswa),
         "persentase_kelulusan": float(graduation_prob),
         "persentase_berprestasi": float(achievement_prob),
-        "keterlibatan_kegiatan": int(keterlibatan_kegiatan)
+        "keterlibatan_kegiatan": int(keterlibatan_kegiatan),
+        "daftar_mata_kuliah": course_list,  # Menambahkan daftar mata kuliah dan kode nilai
+        "daftar_kegiatan": activity_list     # Daftar kegiatan yang diikuti
     }
 
 
